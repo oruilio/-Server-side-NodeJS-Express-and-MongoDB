0097e3cdd5795e12165b5021c915de88044dd622
rui li <rli925@usc.edu>
2019-06-28 12:16:12 -0700

    Simple Node Module

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-var rect = {
-	perimeter: (x, y) => (2*(x+y)),
-	area: (x, y) => (x*y)
-};
+var rect = require('./rectangle');
 
 function solveRect(l,b) {
     console.log("Solving for rectangle with l = " + l + " and b = " + b);
 
     if (l <= 0 || b <= 0) {
         console.log("Rectangle dimensions should be greater than zero:  l = "
-               + l + ",  and b = " + b);
+               + l + ", and b = " + b);
     }
     else {
 	    console.log("The area of the rectangle is " + rect.area(l,b));
